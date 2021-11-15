04ea03d44871e6f158d453e9213e6ca35140d70b
Asuki Kono <asukiaaa@gmail.com>
2021-11-15 10:27:59 +0900

    Apply auto slyling

--- src/BLVD20KM_asukiaaa.cpp
+++ src/BLVD20KM_asukiaaa.cpp
@@ -1,37 +1,38 @@
 #include "BLVD20KM_asukiaaa.h"
 
-#define FN_CODE_READ        0x03
-#define FN_CODE_WRITE       0x06
-#define FN_CODE_DIAGNOSIS   0x08
+#define FN_CODE_READ 0x03
+#define FN_CODE_WRITE 0x06
+#define FN_CODE_DIAGNOSIS 0x08
 #define FN_CODE_WRITE_MULTI 0x10
 
-#define ADDR_ALARM_H         0x0080
-#define ADDR_ALARM_L         0x0081
-#define ADDR_RESET_ALARM_H   0x0180
-#define ADDR_RESET_ALARM_L   0x0181
-#define ADDR_SPEED0_H        0x0480
-#define ADDR_SPEED0_L        0x0481
-#define ADDR_TORQUE_H        0x0700
-#define ADDR_TORQUE_L        0x0701
+#define ADDR_ALARM_H 0x0080
+#define ADDR_ALARM_L 0x0081
+#define ADDR_RESET_ALARM_H 0x0180
+#define ADDR_RESET_ALARM_L 0x0181
+#define ADDR_SPEED0_H 0x0480
+#define ADDR_SPEED0_L 0x0481
+#define ADDR_TORQUE_H 0x0700
+#define ADDR_TORQUE_L 0x0701
 #define ADDR_TORQUE_LIMIT0_H 0x0700
 #define ADDR_TORQUE_LIMIT0_L 0x0701
-#define ADDR_MOTOR_CONTROL   0x007d
-#define ADDR_ANALOG_MODE_L   0x10e3
-#define ADDR_CONFIG_H        0x018c
-#define ADDR_CONFIG_L        0x018d
-#define MOTOR_DIRECTOIN_STOP    0
+#define ADDR_MOTOR_CONTROL 0x007d
+#define ADDR_ANALOG_MODE_L 0x10e3
+#define ADDR_CONFIG_H 0x018c
+#define ADDR_CONFIG_L 0x018d
+#define MOTOR_DIRECTOIN_STOP 0
 #define MOTOR_DIRECTOIN_FORWARD 1
 #define MOTOR_DIRECTOIN_REVERSE 2
 
-#define MOTOR_FORWARD_BIT      B00001000
-#define MOTOR_REVERSE_BIT      B00010000
-#define MOTOR_SLOW_CHANGE_BIT  B00100000
+#define MOTOR_FORWARD_BIT B00001000
+#define MOTOR_REVERSE_BIT B00010000
+#define MOTOR_SLOW_CHANGE_BIT B00100000
 #define MOTOR_FREE_ON_STOP_BIT B10000000
 
 #define DEBUG_PRINT
 
-BLVD20KM_asukiaaa::BLVD20KM_asukiaaa(HardwareSerial* serial, uint8_t address, uint8_t dePin, uint8_t rePin):
-  modbus(serial, dePin, rePin) {
+BLVD20KM_asukiaaa::BLVD20KM_asukiaaa(HardwareSerial *serial, uint8_t address,
+                                     uint8_t dePin, uint8_t rePin)
+    : modbus(serial, dePin, rePin) {
   this->address = address;
 }
 
@@ -51,7 +52,7 @@ uint8_t BLVD20KM_asukiaaa::writeSpeedControlMode(uint16_t mode) {
   if (result != 0) {
     return result;
   }
-  return writeConfigTrigger(); // trigger after setting ADDR_ANALOG_MODE
+  return writeConfigTrigger();  // trigger after setting ADDR_ANALOG_MODE
 }
 
 uint8_t BLVD20KM_asukiaaa::readSpeedControlMode(uint16_t *mode) {
@@ -61,19 +62,23 @@ uint8_t BLVD20KM_asukiaaa::readSpeedControlMode(uint16_t *mode) {
 uint8_t BLVD20KM_asukiaaa::writeConfigTrigger() {
   uint8_t result = writeStop();
   if (result != 0) return result;
+  // return modbus.writeRegisterBy16t(address, ADDR_CONFIG_L, 1);
   return writeRegister(ADDR_CONFIG_L, 1);
 }
 
-uint16_t createMotorControl16bit(uint8_t motorDirection, bool freeLockOnStop = true, bool slowChange = true, uint8_t motorDataNum = 0) {
+uint16_t createMotorControl16bit(uint8_t motorDirection,
+                                 bool freeLockOnStop = true,
+                                 bool slowChange = true,
+                                 uint8_t motorDataNum = 0) {
   // MB-FREE, -, STOP-MODE, REV, FWD, M1, M2, M0
   uint16_t bits = 0x0000;
   switch (motorDirection) {
-  case MOTOR_DIRECTOIN_REVERSE:
-    bits |= MOTOR_REVERSE_BIT;
-    break;
-  case MOTOR_DIRECTOIN_FORWARD:
-    bits |= MOTOR_FORWARD_BIT;
-    break;
+    case MOTOR_DIRECTOIN_REVERSE:
+      bits |= MOTOR_REVERSE_BIT;
+      break;
+    case MOTOR_DIRECTOIN_FORWARD:
+      bits |= MOTOR_FORWARD_BIT;
+      break;
   }
   if (freeLockOnStop) {
     bits |= MOTOR_FREE_ON_STOP_BIT;
@@ -94,28 +99,32 @@ uint8_t BLVD20KM_asukiaaa::writeForward() {
   // uint16_t data16bit = B10101000; // forward and unlock blake
   // uint16_t data16bit = B10001000;
   // return writeRegister(ADDR_MOTOR_CONTROL, data16bit);
-  return writeRegister(ADDR_MOTOR_CONTROL, createMotorControl16bit(MOTOR_DIRECTOIN_FORWARD));
+  return writeRegister(ADDR_MOTOR_CONTROL,
+                       createMotorControl16bit(MOTOR_DIRECTOIN_FORWARD));
 }
 
 uint8_t BLVD20KM_asukiaaa::writeLock() {
 #ifdef DEBUG_PRINT
   Serial.println("lock");
 #endif
-  return writeRegister(ADDR_MOTOR_CONTROL, createMotorControl16bit(MOTOR_DIRECTOIN_STOP, false));
+  return writeRegister(ADDR_MOTOR_CONTROL,
+                       createMotorControl16bit(MOTOR_DIRECTOIN_STOP, false));
 }
 
 uint8_t BLVD20KM_asukiaaa::writeStop() {
 #ifdef DEBUG_PRINT
   Serial.println("stop");
 #endif
-  return writeRegister(ADDR_MOTOR_CONTROL, createMotorControl16bit(MOTOR_DIRECTOIN_STOP));
+  return writeRegister(ADDR_MOTOR_CONTROL,
+                       createMotorControl16bit(MOTOR_DIRECTOIN_STOP));
 }
 
 uint8_t BLVD20KM_asukiaaa::writeReverse() {
 #ifdef DEBUG_PRINT
   Serial.println("reverse");
 #endif
-  return writeRegister(ADDR_MOTOR_CONTROL, createMotorControl16bit(MOTOR_DIRECTOIN_REVERSE));
+  return writeRegister(ADDR_MOTOR_CONTROL,
+                       createMotorControl16bit(MOTOR_DIRECTOIN_REVERSE));
 }
 
 uint8_t BLVD20KM_asukiaaa::writeSpeed(uint16_t speed) {
@@ -144,7 +153,9 @@ uint8_t BLVD20KM_asukiaaa::writeDiagnosis() {
     data[i] = 0;
   }
   result = readQuery(FN_CODE_DIAGNOSIS, data, sizeof(data));
-  if (result != 0) { return result; }
+  if (result != 0) {
+    return result;
+  }
   if (data[2] != 1 || data[3] != 2) {
     return BLVD20KM_ERROR_DIAGNOSIS_DATA_INVALID;
   }
@@ -159,7 +170,6 @@ uint8_t BLVD20KM_asukiaaa::writeResetAlarm() {
   return writeRegister(ADDR_RESET_ALARM_L, 1);
 }
 
-
 uint8_t BLVD20KM_asukiaaa::readAlarm(uint16_t *alarm) {
 #ifdef DEBUG_PRINT
   Serial.println("read alarm");
@@ -167,7 +177,9 @@ uint8_t BLVD20KM_asukiaaa::readAlarm(uint16_t *alarm) {
   return readRegisters(ADDR_ALARM_L, 1, alarm);
 }
 
-uint8_t BLVD20KM_asukiaaa::readDirection(boolean *forwarding, boolean *reversing, boolean *freeLockOnStop) {
+uint8_t BLVD20KM_asukiaaa::readDirection(boolean *forwarding,
+                                         boolean *reversing,
+                                         boolean *freeLockOnStop) {
 #ifdef DEBUG_PRINT
   Serial.println("read direction");
 #endif
@@ -184,14 +196,15 @@ uint8_t BLVD20KM_asukiaaa::readDirection(boolean *forwarding, boolean *reversing
 }
 
 uint16_t uint8tsToUint16t(uint8_t *chars) {
-  return ((uint16_t) chars[0]) << 8 | (uint16_t) chars[1];
+  return ((uint16_t)chars[0]) << 8 | (uint16_t)chars[1];
 }
 
 uint32_t uint16tsToUint32t(uint16_t *shorts) {
-  return ((uint32_t) shorts[0]) << 16 | (uint32_t) shorts[1];
+  return ((uint32_t)shorts[0]) << 16 | (uint32_t)shorts[1];
 }
 
-uint8_t BLVD20KM_asukiaaa::readUint32t(uint16_t readStartAddress, uint32_t *value) {
+uint8_t BLVD20KM_asukiaaa::readUint32t(uint16_t readStartAddress,
+                                       uint32_t *value) {
   uint8_t result;
   result = readRegisters(readStartAddress, 2, uint16Buffer);
   if (result != 0) {
@@ -213,34 +226,38 @@ uint8_t BLVD20KM_asukiaaa::readSpeed(uint16_t *speed) {
   return readRegisters(ADDR_SPEED0_L, 1, speed);
 }
 
-uint8_t BLVD20KM_asukiaaa::writeRegister(uint16_t writeAddress, uint16_t data16bit) {
+uint8_t BLVD20KM_asukiaaa::writeRegister(uint16_t writeAddress,
+                                         uint16_t data16bit) {
   return modbus.writeRegisterBy16t(address, writeAddress, data16bit);
 }
 
-uint8_t BLVD20KM_asukiaaa::readRegisters(uint16_t readStartAddress, uint16_t dataLen, uint16_t* registerData) {
+uint8_t BLVD20KM_asukiaaa::readRegisters(uint16_t readStartAddress,
+                                         uint16_t dataLen,
+                                         uint16_t *registerData) {
   uint8_t result;
-  uint8_t data[] = {
-    highByte(readStartAddress),
-    lowByte(readStartAddress),
-    highByte(dataLen),
-    lowByte(dataLen)
-  };
+  uint8_t data[] = {highByte(readStartAddress), lowByte(readStartAddress),
+                    highByte(dataLen), lowByte(dataLen)};
   writeQuery(FN_CODE_READ, data, sizeof(data));
   result = readQuery(FN_CODE_READ, uint8Buffer, dataLen * 2 + 1);
-  if (result != 0) { return result; }
+  if (result != 0) {
+    return result;
+  }
   // Serial.println("");
   // Serial.println(rDataLen);
   // Serial.println(data16bitLen * 2 + 1);
   for (uint16_t i = 0; i < dataLen; ++i) {
-    registerData[i] = uint8tsToUint16t(&uint8Buffer[i * 2 + 1]); // + 1 to skip data length byte
+    registerData[i] = uint8tsToUint16t(
+        &uint8Buffer[i * 2 + 1]);  // + 1 to skip data length byte
   }
   return 0;
 }
 
-void BLVD20KM_asukiaaa::writeQuery(uint8_t fnCode, uint8_t* data, uint16_t dataLen) {
+void BLVD20KM_asukiaaa::writeQuery(uint8_t fnCode, uint8_t *data,
+                                   uint16_t dataLen) {
   modbus.writeQuery(address, fnCode, data, dataLen);
 }
 
-uint8_t BLVD20KM_asukiaaa::readQuery(uint8_t fnCode, uint8_t* data, uint16_t dataLen) {
+uint8_t BLVD20KM_asukiaaa::readQuery(uint8_t fnCode, uint8_t *data,
+                                     uint16_t dataLen) {
   return modbus.readQuery(address, fnCode, data, dataLen);
 }
--- src/BLVD20KM_asukiaaa.h
+++ src/BLVD20KM_asukiaaa.h
@@ -6,16 +6,16 @@
 
 #define BLVD20KM_QUERY_MAX_LEN 41
 
-#define BLVD20KM_ERROR_CODE_INVALID_FN        0x01
-#define BLVD20KM_ERROR_CODE_INVALID_ADDR      0x02
-#define BLVD20KM_ERROR_CODE_INVALID_DATA      0x03
-#define BLVD20KM_ERROR_CODE_SLAVE_ERROR       0x04
-#define BLVD20KM_ERROR_NO_RESPONSE            0x10
-#define BLVD20KM_ERROR_UNMATCH_CRC            0x11
-#define BLVD20KM_ERROR_UNMATCH_ADDRESS        0x12
-#define BLVD20KM_ERROR_UNMATCH_FN_CODE        0x13
-#define BLVD20KM_ERROR_UNMATCH_DATA_LEN       0x14
-#define BLVD20KM_ERROR_OVER_QUERY_MAX_LEN     0x15
+#define BLVD20KM_ERROR_CODE_INVALID_FN 0x01
+#define BLVD20KM_ERROR_CODE_INVALID_ADDR 0x02
+#define BLVD20KM_ERROR_CODE_INVALID_DATA 0x03
+#define BLVD20KM_ERROR_CODE_SLAVE_ERROR 0x04
+#define BLVD20KM_ERROR_NO_RESPONSE 0x10
+#define BLVD20KM_ERROR_UNMATCH_CRC 0x11
+#define BLVD20KM_ERROR_UNMATCH_ADDRESS 0x12
+#define BLVD20KM_ERROR_UNMATCH_FN_CODE 0x13
+#define BLVD20KM_ERROR_UNMATCH_DATA_LEN 0x14
+#define BLVD20KM_ERROR_OVER_QUERY_MAX_LEN 0x15
 #define BLVD20KM_ERROR_DIAGNOSIS_DATA_INVALID 0x16
 
 #define BLVD20KM_SPEED_MIN 80
@@ -26,7 +26,8 @@
 
 class BLVD20KM_asukiaaa {
  public:
-  BLVD20KM_asukiaaa(HardwareSerial* serial, uint8_t address, uint8_t dePin, uint8_t rePin);
+  BLVD20KM_asukiaaa(HardwareSerial *serial, uint8_t address, uint8_t dePin,
+                    uint8_t rePin);
   void begin(int baudrate, int config = SERIAL_8E1);
 
   uint8_t writeForward();
@@ -34,7 +35,8 @@ class BLVD20KM_asukiaaa {
   uint8_t writeStop();
   uint8_t writeReverse();
 
-  uint8_t readDirection(boolean *forwarding, boolean *reversing, boolean *freeLockOnStop);
+  uint8_t readDirection(boolean *forwarding, boolean *reversing,
+                        boolean *freeLockOnStop);
   uint8_t readSpeed(uint16_t *speed);
   uint8_t readSpeedControlMode(uint16_t *mode);
   uint8_t readTorque(uint16_t *torque);
@@ -54,11 +56,12 @@ class BLVD20KM_asukiaaa {
   // uint8_t rePin;
   uint8_t queryBuffer[BLVD20KM_QUERY_MAX_LEN];
   uint8_t readUint32t(uint16_t readStartAddress, uint32_t *value);
-  uint8_t readQuery(uint8_t fnCode, uint8_t* data, uint16_t dataLen);
-  uint8_t readRegisters(uint16_t readStartAddress, uint16_t dataLen, uint16_t* registerData);
+  uint8_t readQuery(uint8_t fnCode, uint8_t *data, uint16_t dataLen);
+  uint8_t readRegisters(uint16_t readStartAddress, uint16_t dataLen,
+                        uint16_t *registerData);
   uint8_t writeConfigTrigger();
   uint8_t writeRegister(uint16_t writeAddress, uint16_t data16bit);
-  void writeQuery(uint8_t fnCode, uint8_t* data, uint16_t dataLen);
+  void writeQuery(uint8_t fnCode, uint8_t *data, uint16_t dataLen);
 
   uint16_t uint16Buffer[8];
   uint8_t uint8Buffer[41];
